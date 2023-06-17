0b02089dbad55d9bb4adf9e92604bbfad957464d
Wyatt Barnes <me@wyatt.email>
2023-06-17 11:09:26 -1000

    Replace yarn usage with NPM

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { Web3 } from "web3";
+const Web3 = require("web3").default;
 
-const web3 = new Web3("https://yourProvider.com");
+const web3 = new Web3("http://localhost:1234");
 web3.eth
   .getBalance("0x05fB80699034011c702B7e1C8429f2977E4C6B63")
   .then((balance) => console.log(balance));
